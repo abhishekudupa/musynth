907360ebb3d6f87fcb35ea88ecbac686225bdb3d
Abhishek Udupa <audupa@seas.upenn.edu>
2014-03-13 20:31:40 -0400

    started implementation of checker

---
+++ src/musynthASTChecker.ml
@@ -0,0 +1,19 @@
+(* semantic checks on AST *)
+
+open MusynthAST
+
+let gatherQVarsInDesig desig =
+  let retval = ref [] in
+  let rec gatherQVarsInDesigRec desig =
+    match desig with
+    | SimpleDesignator _ -> ()
+    | IndexDesignator (innerdesig, ident) ->
+        retval := ident :: !retval;
+        gatherQVarsInDesigRec innerdesig
+    | FieldDesignator (innerdesig, ident) ->
+        gatherQVarsInDesigRec innerdesig
+  in
+  gatherQVarsInDesigRec desig;
+  !retval
+
+
